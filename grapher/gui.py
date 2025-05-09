6757901c753089237accb95c5c3689ebb9015766
vkirs229037 <vova.kirsanov.03@mail.ru>
2025-05-09 16:28:06 +0300

    + 2 действия

--- grapher/gui.py
+++ grapher/gui.py
@@ -78,6 +78,10 @@ class MainWindow(QMainWindow):
         bfs_action.triggered.connect(lambda checked, arg="bfs": self.insert_alg(checked, arg))
         dfs_action = QAction("Поиск в глубину", self)
         dfs_action.triggered.connect(lambda checked, arg="dfs": self.insert_alg(checked, arg))
+        all_trees_action = QAction("Все деревья графа", self)
+        all_trees_action.triggered.connect(lambda checked, arg="alltrees": self.insert_alg(checked, arg))
+        chrom_num_action = QAction("Хроматическое число графа", self)
+        chrom_num_action.triggered.connect(lambda checked, arg="chromnum": self.insert_alg(checked, arg))
         
         algMenu.addActions([dijkstra_action, floyd_action, chinese_post_action, max_match_action, max_indep_action, hakimi_action, neg_cycle_action, bfs_action, dfs_action])
 
