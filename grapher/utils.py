3a8cde2bc887323b78bc2d9134964c8f1ef08d78
vkirs229037 <vova.kirsanov.03@mail.ru>
2025-05-20 13:32:46 +0300

    Поменял функцию для случайного цвета

--- grapher/utils.py
+++ grapher/utils.py
@@ -2,4 +2,4 @@ import numpy as np
 from matplotlib import colors
 
 def random_color():
-    return colors.hsv_to_rgb((np.random.rand(), np.random.rand(), 1))
+    return colors.hsv_to_rgb((np.random.rand(), (np.random.rand() + 1) / 2, 1))
