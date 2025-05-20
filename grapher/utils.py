2dee1945276570dddcb286caa097ab5382c77164
vkirs229037 <vova.kirsanov.03@mail.ru>
2025-05-20 22:35:09 +0300

    функция для создания случайного связного графа

--- grapher/utils.py
+++ grapher/utils.py
@@ -8,3 +8,24 @@ def gen_palette():
     vals = np.linspace(0.75, 1, 2)
     cols = sorted(map(lambda t: colors.hsv_to_rgb(t), itertools.product(hues, sats, vals)), key=lambda t: t[1])
     return list(cols)
+
+def gen_random_graph(n):
+    s = "vertex {\n"
+    vs = range(1, n+1)
+    for i in vs:
+        s += "\t" + str(i) + ";\n"
+    s += "}\ngraph {\n\tundirected;\n"
+    for i in vs:
+        stmt = f"\t{i} > "
+        l = np.random.rand() > 0.5
+        if l:
+            stmt += "["
+            for j in vs:
+                if np.random.rand() > 0.5:
+                    stmt += str(j) + " "
+            stmt += "]"
+        else:
+            stmt += str(np.random.randint(1, n+1))
+        s += stmt + " #1;\n"
+    s += "}"
+    return s
